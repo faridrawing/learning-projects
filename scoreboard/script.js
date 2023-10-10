9a33d2c0f7586259cc49b02a85a39ff49070c032
Muhammad Farid Yamin <97384053+faridrawing@users.noreply.github.com>
2023-10-10 07:34:51 +0700

    Create script.js

---
+++ scoreboard/script.js
@@ -0,0 +1,36 @@
+const scoreLeft = document.querySelector(".score-left");
+const scoreRight = document.querySelector(".score-right");
+
+scoreLeft.querySelector("span p").addEventListener("click", () => {
+  const name = prompt("Enter your name : ");
+  scoreLeft.querySelector("span h1").textContent = name == "" || name == null ? "(Team Name)" : name;
+});
+
+scoreLeft.querySelector("span button.plus").addEventListener("click", () => {
+  let score = parseInt(scoreLeft.querySelector("span.number").textContent);
+  score++;
+  scoreLeft.querySelector("span.number").textContent = score;
+});
+
+scoreLeft.querySelector("span button.minus").addEventListener("click", () => {
+  let score = parseInt(scoreLeft.querySelector("span.number").textContent);
+  score--;
+  scoreLeft.querySelector("span.number").textContent = score;
+});
+
+scoreRight.querySelector("span p").addEventListener("click", () => {
+  const name = prompt("Enter your name : ");
+  scoreRight.querySelector("span h1").textContent = name == "" || name == null ? "(Team Name)" : name;
+});
+
+scoreRight.querySelector("span button.plus").addEventListener("click", () => {
+  let score = parseInt(scoreRight.querySelector("span.number").textContent);
+  score++;
+  scoreRight.querySelector("span.number").textContent = score;
+});
+
+scoreRight.querySelector("span button.minus").addEventListener("click", () => {
+  let score = parseInt(scoreRight.querySelector("span.number").textContent);
+  score--;
+  scoreRight.querySelector("span.number").textContent = score;
+});
